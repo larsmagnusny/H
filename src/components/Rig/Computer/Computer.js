80d229d40849f59059b8469f3167386623982aae
Lars Magnus Nyland <lars.magnus.nyland@gmail.com>
2021-06-27 21:36:51 +0200

    Added the ability to display the current hardware

--- src/components/Rig/Computer/Computer.js
+++ src/components/Rig/Computer/Computer.js
@@ -3,55 +3,31 @@ import Select from 'react-select'
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faDesktop } from "@fortawesome/free-solid-svg-icons";
+import { faDesktop, faMicrochip, faMemory, faPlus, faFilm, faNetworkWired, faHdd} from "@fortawesome/free-solid-svg-icons";
 
 import './Computer.css';
 
 library.add(faDesktop);
-
-const CPUOptions = [
-    {value: 1, label: '200Mhz'},
-    {value: 2, label: '600Mhz'},
-    {value: 3, label: '1 Ghz'},
-    {value: 4, label: '2 Ghz'},
-    {value: 5, label: '4 Ghz'},
-    {value: 6, label: '6 Ghz'},
-    {value: 7, label: '10 Ghz'},
-    {value: 8, label: '16 Ghz'},
-    {value: 9, label: '24 Ghz'}
-];
-
-const RAMOptions = [
-    {value: 1, label: '128MB'},
-    {value: 2, label: '256MB'},
-    {value: 3, label: '1GB'},
-    {value: 4, label: '4GB'},
-    {value: 5, label: '16GB'},
-    {value: 6, label: '32GB'},
-    {value: 7, label: '64GB'},
-    {value: 8, label: '128GB'},
-    {value: 9, label: '256GB'},
-    {value: 10, label: '1TB'}
-];
-
-const HDDOptions = [
-    {value: 1, label: '1GB'},
-    {value: 2, label: '50GB'},
-    {value: 3, label: '150GB'},
-    {value: 4, label: '500GB'},
-    {value: 5, label: '1TB'},
-    {value: 6, label: '2TB'},
-    {value: 7, label: '10TB'},
-    {value: 8, label: '50TB'},
-    {value: 9, label: '250TB'},
-    {value: 10, label: '500TB'},
-    {value: 11, label: '1PB'}
-];
+library.add(faMicrochip);
+library.add(faMemory);
+library.add(faPlus);
+library.add(faFilm);
+library.add(faNetworkWired);
+library.add(faHdd);
 
 class Computer extends Component
 {
+    constructor(props){
+        super(props);
+    }
+
     render(){
-        const {title} = this.props;
+        const {data} = this.props;
+
+        const freeCpuSlots = Array.from({length: data.cpuSlots -  data.cpus.length}, (v, i) => i);
+        const freeRamSlots = Array.from({length: data.ramSlots -  data.rams.length}, (v, i) => i);
+        const freeHddSlots = Array.from({length: data.sataSlots -  data.hdds.length}, (v, i) => i);
+        const freePciSlots = Array.from({length: data.pciSlots -  data.gpus.length - data.nets.length}, (v, i) => i);
 
         return(
         <div className="computerNode">
@@ -59,10 +35,82 @@ class Computer extends Component
                 <FontAwesomeIcon icon="desktop" size="3x" />
             </div>
             <div className="computerContents">
-                <h3>{title}</h3>
-                <Select options={CPUOptions} />
-                <Select options={RAMOptions} />
-                <Select options={HDDOptions} />
+                <h3>{data.name}</h3>
+                <div className="slots">
+                    <label>CPU slots</label>
+                    <div className="slotsContent">
+                        {data.cpus.map((cpu) => (
+                            <div className="slot" title={cpu.name}>
+                                <FontAwesomeIcon icon="microchip" />
+                            </div>
+                        ))}
+                        {freeCpuSlots.map(() => (
+                            <div className="slot">
+                                <FontAwesomeIcon icon="plus" />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="slots">
+                    <label>RAM slots</label>
+                    <div className="slotsContent">
+                        {data.rams.map((ram) => (
+                            <div className="slot" title={ram.name}>
+                                <FontAwesomeIcon icon="memory" />
+                            </div>
+                        ))}
+                        {freeRamSlots.map(() => (
+                            <div className="slot">
+                                <FontAwesomeIcon icon="plus" />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="slots">
+                    <label>HDD slots</label>
+                    <div className="slotsContent">
+                        {data.hdds.map((hdd) => (
+                            <div className="slot" title={hdd.name}>
+                                <FontAwesomeIcon icon="hdd" />
+                            </div>
+                        ))}
+                        {freeHddSlots.map(() => (
+                            <div className="slot">
+                                <FontAwesomeIcon icon="plus" />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="slots">
+                    <label>GPU slots</label>
+                    <div className="slotsContent">
+                        {data.gpus.map((ram) => (
+                            <div className="slot" title={ram.name}>
+                                <FontAwesomeIcon icon="film" />
+                            </div>
+                        ))}
+                        {freePciSlots.map(() => (
+                            <div className="slot">
+                                <FontAwesomeIcon icon="plus" />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="slots">
+                    <label>Network card slots</label>
+                    <div className="slotsContent">
+                        {data.nets.map((ram) => (
+                            <div className="slot" title={ram.name}>
+                                <FontAwesomeIcon icon="network-wired" />
+                            </div>
+                        ))}
+                        {freePciSlots.map(() => (
+                            <div className="slot">
+                                <FontAwesomeIcon icon="plus" />
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>);
     }
--- src/components/Rig/Rig.js
+++ src/components/Rig/Rig.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import Computer from './Computer/Computer';
 
-export default function Rig(){
-    return(
-        <div>
-            <h1>Local network</h1>
-            <Computer title="test" />
-        </div>
-    );
+function getToken(){
+    const tokenString = sessionStorage.getItem('token');
+    const userToken = JSON.parse(tokenString);
+    
+    return userToken?.token;
+  }
+
+export default class Rig extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    componentWillMount(){
+        fetch('https://hloginapi.azurewebsites.net/computer?token=' + getToken(), {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+          .then(data => data.json())
+          .then(data => {
+              this.setState({ computers: data });
+          });
+    }
+    
+    render(){
+        if(this.state == null){
+            return(<div>Loading...</div>)
+        }
+
+        const { computers } = this.state;
+
+        return(
+            <div>
+                <h1>Local network</h1>
+                {computers.map((data) => (
+                    <Computer data={data} />
+                ))}
+            </div>
+        );
+    }
 }
